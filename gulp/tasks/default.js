5b8ddb5e329b44fd6c44c229a3f06b40f1c1006d
Scott Mathias Sarsfield <scott.m.sarsfield@gmail.com>
2017-04-12 21:23:05 -0700

    Checking tests too often... (when working with UI)

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -3,5 +3,5 @@ var gulp = require('gulp');
 gulp.task('default',['mocha','javascript','static-files','renderStatic','browserSync'],function(){
     gulp.watch(['www/**'],['static-files']);
     gulp.watch(['templates/**','src/**'],[/*'renderStatic',*/'javascript']);
-    gulp.watch(['src/**','test/**'],['mocha']);
+    //gulp.watch(['src/**','test/**'],['mocha']); // don't need to constantly test it...
 });
